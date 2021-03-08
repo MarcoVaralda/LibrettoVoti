817da4b22140bdfdd6674f5b2b7ef3ae408f578e
marco <marco@192.168.1.4>
2021-03-08 11:20:13 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
@@ -1,10 +1,15 @@
 package it.polito.tdp.librettovoti.model;
 
 import java.time.LocalDate;
+import java.util.*;
 
 public class TestLibretto {
 	
 	public static void main(String[] args) {
+		
+		// Punto 1
+		
+		System.out.println("***** PUNTO 1 *****\n");
 		
 		System.out.println("Test metodi di libretto:");		
 		Libretto libretto = new Libretto();
@@ -12,11 +17,25 @@ public class TestLibretto {
 		Voto voto1 = new Voto("Analisi 1", 30, LocalDate.of(2019, 2, 15));
 		libretto.add(voto1);
 		
+		libretto.add(new Voto("Chimica", 25, LocalDate.of(2019, 9, 15)));
 		libretto.add(new Voto("Fisica 1", 28, LocalDate.of(2019, 7, 15)));
 		libretto.add(new Voto("Informatica", 24, LocalDate.of(2019, 9, 15)));
 		
-		System.out.println(libretto);	
+		System.out.println(libretto);
 		
+		// Punto 2
+		
+		System.out.println("***** PUNTO 2 *****\n");
+		
+		// Prima soluzione con le liste (Nella stampa ho però le parentesi quadre
+		/*
+		List<Voto> venticinque = libretto.listaVotiUguali(25);
+		System.out.println(venticinque);
+		*/
+
+		// Seconda soluzione con il libretto ( migliore perchè ho un metodo di stampa implementato da me
+		Libretto librettoVenticinque = libretto.votiUguali(25);
+		System.out.println(librettoVenticinque);
 	}	
 
 }
